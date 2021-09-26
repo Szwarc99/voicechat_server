7566d7df2ee173676dc649a58483926961a135e8
Szwarc99 <51268010+Szwarc99@users.noreply.github.com>
2021-09-26 15:22:03 +0200

    fixed high CPU usage

---
+++ VoiceChatServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace MemoryServer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ServerTCP server = new ServerTCP(8080);
+                server.LoopClients();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
