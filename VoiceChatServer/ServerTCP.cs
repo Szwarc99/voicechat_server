6247735bfb71a7b458bcdc9006826fff86efe7e4
Szwarc99 <51268010+Szwarc99@users.noreply.github.com>
2021-09-30 23:24:11 +0200

    add unique usernames

--- VoiceChatServer/ServerTCP.cs
+++ VoiceChatServer/ServerTCP.cs
@@ -17,7 +17,7 @@ namespace VoiceChatServer
         private TcpListener _server;
         private Boolean _isRunning;
 
-        private List<string> loggedUsers = new List<string>();
+        private HashSet<string> loggedUsers = new HashSet<string>();
 
         private ConcurrentDictionary<int, Room> roomDict = new ConcurrentDictionary<int, Room>();
         private ConcurrentDictionary<string, int> roomOwners = new ConcurrentDictionary<string, int>();
@@ -39,7 +39,7 @@ namespace VoiceChatServer
             while (_isRunning)
             {
                 // wait for client connection                
-                TcpClient newClient = _server.AcceptTcpClient();                
+                TcpClient newClient = _server.AcceptTcpClient();
                 // client found.
                 // create a thread to handle communication
 
@@ -58,89 +58,110 @@ namespace VoiceChatServer
                 });
                 t.Start();
             }
-        }       
+        }
 
         public void HandleClient(TcpClient client)
         {
             NetworkStream stream = client.GetStream();
             CommProtocol.setAes(stream);
-            
+
             bool clientConnected = true;
+            bool logged = false;
             string userID = "";
 
             CommProtocol.Write(stream, "test");
             while (clientConnected)
-            { 
-                string sData = "";
-                try
-                {                    
-                    sData = CommProtocol.Read(stream);
+            {
+
+                do
+                {
+                    string sData = CommProtocol.Read(stream);
                     Console.WriteLine(sData);
-                }
-                catch (Exception e)
-                {                    
-                    sData = "dsc";
-                }
-                string[] logData = CommProtocol.CheckMessage(sData);
+                    string[] logData = CommProtocol.CheckMessage(sData);
+                    if (logData[0] == "con")
+                    {
+                        userID = logData[1];
+                        lock (loggedUsers)
+                        {
+                            if (loggedUsers.Contains(userID))
+                            {
+                                CommProtocol.Write(stream, "error username_already_taken");
+                            }
+                            else
+                            {
+                                logged = true;
+                                loggedUsers.Add(userID);
+                                CommProtocol.Write(stream, "con ok");
+                            }
+                        }
+                    }
+                    else Console.WriteLine("wrong command");
+                } while (!logged);
 
-                var rooms = this.roomDict.ToArray().Select(x => x.Value).ToList();
-                rooms.Sort((x, y) => x.id - y.id);
+                while (logged)
+                {
+                    string sData = "";
+                    try
+                    {
+                        sData = CommProtocol.Read(stream);
+                        Console.WriteLine(sData);
+                    }
+                    catch (Exception e)
+                    {
+                        sData = "dsc";
+                    }
+                    string[] logData = CommProtocol.CheckMessage(sData);
 
-                if (sData == "dsc")
-                {
-                    clientConnected = false;
-                    lock(loggedUsers)
-                    {
-                        loggedUsers.Remove(userID);
-                    }
-                }
-                if (logData[0] == "con")
-                {                    
-                    userID = logData[1];
-                    lock (loggedUsers)
-                    {
-                        loggedUsers.Add(userID);
-                    }                    
-                    CommProtocol.Write(stream, "con ok");
-                }
-                if (logData[0] == "ref")
-                {
-                    StringBuilder sb = new StringBuilder();
-                    sb.Append(rooms.Count);
-                    foreach (Room room in rooms)
-                    {
-                        sb.Append(room.Encode());
-                    }
-                    CommProtocol.Write(stream, sb.ToString());
-                }
-                else if (logData[0] == "crm")
-                {
-                    if (!roomOwners.ContainsKey(userID))
-                    {
-                        int id = getNextId();
-                        roomDict.TryAdd(id, new Room(id, bool.Parse(logData[1]), logData[2]));
-                        roomOwners.TryAdd(userID, id);
-                        string str = "crm " + id.ToString();
-                        CommProtocol.Write(stream, str);
-                    }
-                    else CommProtocol.Write(stream, "error room_already_created");
-                }
-                else if (logData[0] == "jrm")
-                {
-                    string pwd = "";
-                    int port;
+                    var rooms = this.roomDict.ToArray().Select(x => x.Value).ToList();
+                    rooms.Sort((x, y) => x.id - y.id);
 
-                    if (logData.Length == 5)
+                    if (sData == "dsc")
                     {
-                        pwd = logData[3];
-                        port = Int32.Parse(logData[4]);
+                        clientConnected = false;
+                        lock (loggedUsers)
+                        {
+                            loggedUsers.Remove(userID);
+                        }
                     }
-                    else
-                        port = Int32.Parse(logData[3]);
-                    Console.WriteLine(port);
-                    rooms[int.Parse(logData[1])].HandleClient(client, logData[2], pwd, port);
-                }                
-            }            
+                    if (logData[0] == "ref")
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append(rooms.Count);
+                        foreach (Room room in rooms)
+                        {
+                            sb.Append(room.Encode());
+                        }
+                        CommProtocol.Write(stream, sb.ToString());
+                    }
+                    else if (logData[0] == "crm")
+                    {
+                        if (!roomOwners.ContainsKey(userID))
+                        {
+                            int id = getNextId();
+                            roomDict.TryAdd(id, new Room(id, bool.Parse(logData[1]), logData[2]));
+                            roomOwners.TryAdd(userID, id);
+                            string str = "crm " + id.ToString();
+                            CommProtocol.Write(stream, str);
+                        }
+                        else CommProtocol.Write(stream, "error room_already_created");
+                    }
+                    else if (logData[0] == "jrm")
+                    {
+                        string pwd = "";
+                        int port;
+
+                        if (logData.Length == 5)
+                        {
+                            pwd = logData[3];
+                            port = Int32.Parse(logData[4]);
+                        }
+                        else
+                            port = Int32.Parse(logData[3]);
+                        Console.WriteLine(port);
+                        rooms[int.Parse(logData[1])].HandleClient(client, logData[2], pwd, port);
+                    }
+                }
+            }
         }
     }
 }
